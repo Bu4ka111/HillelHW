40a6b36d7528934313dfa5d7a9a8e8851fdae819
MDrago <nikolatesla212001@gmail.com>
2026-01-13 23:55:42 +0200

    Добавил отношение парных к непарнам

--- js_homework4.js
+++ js_homework4.js
@@ -1,26 +1,31 @@
 function checkProbabilityTheory(count) {
-    let evenCount = 0;
-    let oddCount = 0;
-  
-    for (let i = 0; i < count; i++) {
-      let randomNumber = Math.floor(Math.random() * 901) + 100;
-      
-      if (randomNumber % 2 === 0) {
-        evenCount++;
-      } else {
-        oddCount++;
-      }
+  let evenCount = 0;
+  let oddCount = 0;
+
+  for (let i = 0; i < count; i++) {
+    let randomNumber = Math.floor(Math.random() * 900) + 100;
+
+    if (randomNumber % 2 === 0) {
+      evenCount++;
+    } else {
+      oddCount++;
     }
-  
-    let evenPercentage = (evenCount / count) * 100;
-    let oddPercentage = (oddCount / count) * 100;
-  
-    console.log('Кількість згенерованих чисел: ' + count);
-    console.log('Парних чисел: ' + evenCount);
-    console.log('Не парних чисел: ' + oddCount);
-    console.log('Відсоток парних: ' + evenPercentage.toFixed(2) + '%');
-    console.log('Відсоток непарних: ' + oddPercentage.toFixed(2) + '%');
-    console.log('Відсоток парних до непарних: ' + evenPercentage.toFixed(2) + '%' + "/" + oddPercentage.toFixed(2) + '%');
   }
 
-  checkProbabilityTheory(1000)
+  let evenPercentage = (evenCount / count) * 100;
+  let oddPercentage = (oddCount / count) * 100;
+
+  let evenToOddPercentage = 0;
+  if (oddCount !== 0) {
+    evenToOddPercentage = (evenCount / oddCount) * 100;
+  }
+
+  console.log('Кількість згенерованих чисел: ' + count);
+  console.log('Парних чисел: ' + evenCount);
+  console.log('Непарних чисел: ' + oddCount);
+  console.log('Відсоток парних: ' + evenPercentage.toFixed(2) + '%');
+  console.log('Відсоток непарних: ' + oddPercentage.toFixed(2) + '%');
+  console.log('Відсоток парних до непарних: ' + evenToOddPercentage.toFixed(2) + '%' );
+}
+
+checkProbabilityTheory(1000);
